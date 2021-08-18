93fbdeee17523b589a652afdfb8476372221911e
TAMIM <saifur.tamim@gmail.com>
2021-08-18 11:29:56 +0600

    quantity +,- button working

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('I am in a separate file')
+
+const productQuantity = document.querySelectorAll('.product-quantity');
+
+productQuantity.forEach(element => {
+    element.addEventListener('click', productQuantityFunction);
+});
+
+function productQuantityFunction(event) {
+    const quantityContainer = this.querySelector('.quantity')
+    const previousQuantity = parseInt(quantityContainer.value);
+    if (event.target.classList[0] == 'quantity-plus') {
+        quantityContainer.value = previousQuantity + 1;
+    } else if (event.target.classList[0] == 'quantity-minus'
+        && previousQuantity > 0) {
+        quantityContainer.value = previousQuantity - 1;
+    }
+}
