3d40dbcff9c7b8030f1ce6749c7bf45a4a8781c9
TAMIM <saifur.tamim@gmail.com>
2021-08-18 12:05:40 +0600

    fully functional shopping cart
    price update function added

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 
-const productQuantity = document.querySelectorAll('.product-quantity');
+const productDetails = document.querySelectorAll('.product-price-details');
 
-productQuantity.forEach(element => {
-    element.addEventListener('click', productQuantityFunction);
+productDetails.forEach(element => {
+    element.addEventListener('click', productDetailsUpdate);
 });
 
-function productQuantityFunction(event) {
-    const quantityContainer = this.querySelector('.quantity')
+function productDetailsUpdate(event) {
+    const quantityContainer = this.querySelector('.quantity');
+    const priceContainer = this.querySelector('.product-price');
+    const productPrice = parseInt(priceContainer.classList[1]);
     const previousQuantity = parseInt(quantityContainer.value);
+    let newQuantity;
     if (event.target.classList[0] == 'quantity-plus') {
-        quantityContainer.value = previousQuantity + 1;
+        newQuantity = previousQuantity + 1;
     } else if (event.target.classList[0] == 'quantity-minus'
         && previousQuantity > 0) {
-        quantityContainer.value = previousQuantity - 1;
+        newQuantity = previousQuantity - 1;
     }
+    quantityContainer.value = newQuantity;
+    priceContainer.innerText = newQuantity * productPrice;
+    updateTotal();
+}
+
+function updateTotal() {
+    const productsPrices = document.querySelectorAll('.product-price');
+    let subTotal = 0;
+    for (const price of productsPrices) {
+        subTotal += parseInt(price.innerText);
+    }
+    document.getElementById('sub-total').innerText = subTotal;
+    const tax = (subTotal * .05).toFixed(2);
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-amount').innerText = (subTotal + parseFloat(tax)).toFixed(2);
 }
